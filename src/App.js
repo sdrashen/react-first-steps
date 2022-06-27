c00ac0d880a8263cb95af062a5e7d65a148e1372
Sabrina Drashen <sdrashen@gmail.com>
2022-06-27 15:17:28 -0300

    created Tasks component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import React, { useState } from 'react'
+import Tasks from './components/tasks'
+
 import './App.css'
 
 const App = () => {
-  //   let message = 'Hello World!'
   // A var normal quando alterada não atualiza o componente
-  const [message, setMessage] = useState('Hello world!')
   //O state atualiza o componente
+  const [tasks, setTasks] = useState([
+    {
+      id: '1',
+      title: 'Estudar programação',
+      completed: false
+    },
+    {
+      id: '2',
+      title: 'Ler livros',
+      completed: true
+    }
+  ])
   return (
     <>
-      <div className="container"> {message} </div>
-      <button onClick={() => setMessage('Hello')}> Mudar mensagem </button>
+      <div className="container">
+        <Tasks />
+      </div>
     </>
   )
 }
