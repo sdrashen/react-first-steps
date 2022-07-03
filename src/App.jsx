cd717d42e7a20196d94d2e1edbf4c62498049883
Sabrina Drashen <sdrashen@gmail.com>
2022-07-03 14:26:13 -0300

    added a task inserted by the user

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { v4 as uuidv4 } from 'uuid'
 import Tasks from './components/Tasks'
 
 import './App.css'
@@ -19,10 +20,26 @@ const App = () => {
       completed: true
     }
   ])
+
+  const handleTaskAddition = taskTitle => {
+    //Vai add o título no useState
+    const newTasks = [
+      ...tasks,
+      {
+        title: taskTitle,
+        //Foi instalada e importada a biblioteca uuid para gerar uma id aleatória. Assim, podemos
+        //excluir o Math.random(10)
+        //id: Math.random(10),
+        id: uuidv4(),
+        completed: false
+      }
+    ]
+    setTasks(newTasks)
+  }
   return (
     <>
       <div className="container">
-        <AddTask />
+        <AddTask handleTaskAddition={handleTaskAddition} />
         <Tasks tasks={tasks} />
       </div>
     </>
@@ -30,7 +47,7 @@ const App = () => {
 }
 
 //Props são dados que podem ser passados de um componente pai para um componente filho
-//Nesse caso, ao App é o comp pai da Tasks, porque ele está renderizando a Tasks
+//Nesse caso, ao App é o componente pai da Tasks, porque ele está renderizando a Tasks
 //No file tasks.js recebe um parâmetro chamado props
 
 export default App
--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,14 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Button from './Button'
 
 import './AddTask.css'
 
-const AddTask = () => {
+const AddTask = ({ handleTaskAddition }) => {
+  //Esse AddTask pega o useState e add na lista de tarefas do file App.jsx
+  const [inputData, setInputData] = useState('')
+  const handleInputChange = e => {
+    setInputData(e.target.value)
+  }
+
+  const handleAddTaskClick = () => {
+    //Vai pegar o que for inserido pelo usuário e colocar no inputData
+    //quando clicar em "Adicionar", ela vai pegar o que estiver no inputData abaixo
+    //e mandar para o taskAdition que por sua vez vai pegar o inputData
+    //que nesse caso será o taskTitle e vai add nas tasks e setar o state.
+    handleTaskAddition(inputData)
+    setInputData('')
+  }
+
   return (
     <div className="add-task-container">
-      <input className="add-task-input" type="text" />
+      <input
+        onChange={handleInputChange}
+        value={inputData}
+        className="add-task-input"
+        type="text"
+      />
       <div className="add-task-button-container">
-        <Button>Adicionar</Button>
+        <Button onClick={handleAddTaskClick}>Adicionar</Button>
       </div>
     </div>
   )
