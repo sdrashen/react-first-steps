5b61e84ca3dc65467afc4c95767e0e226de4b6ae
Sabrina Drashen <sdrashen@gmail.com>
2022-07-08 09:01:16 -0300

    configured reatc-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { v4 as uuidv4 } from 'uuid'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
 
@@ -56,17 +57,26 @@ const App = () => {
   }
 
   return (
-    <>
+    <Router>
       <div className="container">
         <Header />
-        <AddTask handleTaskAddition={handleTaskAddition} />
-        <Tasks
-          tasks={tasks}
-          handleTaskClick={handleTaskClick}
-          handleTaskDeletion={handleTaskDeletion}
+        <Route
+          path="/"
+          exact
+          render={() => (
+            <>
+              <AddTask handleTaskAddition={handleTaskAddition} />
+              <Tasks
+                tasks={tasks}
+                handleTaskClick={handleTaskClick}
+                handleTaskDeletion={handleTaskDeletion}
+              />
+            </>
+          )}
         />
       </div>
-    </>
+    </Router>
+    //Precisamos fazer o mapeamento de cada "page" para os nossos componentes
   )
 }
 
